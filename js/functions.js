d6e61be9b6c321b56de6a636c208491e3bb732cc
ваше имя <igorsychkin04@gmail.com>
2024-12-05 13:50:59 +0500

    5.16_v1

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,22 @@
-function isStringMaxLength(str, maxLength) {
-  return str.length<= maxLength;
+function parseTime(time) {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
 }
-console.log(isStringMaxLength('проверяемая строка', 20));
-console.log(isStringMaxLength('проверяемая строка', 18));
-console.log(isStringMaxLength('проверяемая строка', 10));
 
-function isPalindrome(str) {
-  const lowerString = str.toLowerCase().replaceAll(' ', '');
-  const reversedStr= lowerString.split('').reverse().join('');
-  return lowerString === reversedStr;
+
+function defineTime(startTime, endTime, startSpendingTime, spendingTimeMinutes) {
+  const startWork = parseTime(startTime);
+  const endWork = parseTime(endTime);
+  const startMeeting = parseTime(startSpendingTime);
+  const endMeeting = startMeeting + spendingTimeMinutes;
+
+  return startMeeting >= startWork && endMeeting <= endWork;
 }
-console.log(isPalindrome('топот'));
-console.log(isPalindrome('ДовОд'));
-console.log(isPalindrome('Кекс'));
-console.log(isPalindrome('Лёша на полке клопа нашёл'));
+
+// Примеры использования
+defineTime('08:00', '17:30', '14:00', 90);
+defineTime('8:0', '10:0', '8:0', 120);
+defineTime('08:00', '14:30', '14:00', 90);
+defineTime('14:00', '17:30', '08:0', 90);
+defineTime('8:00', '17:30', '08:00', 900);
 
